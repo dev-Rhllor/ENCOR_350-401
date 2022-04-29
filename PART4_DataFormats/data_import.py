b1fd6069fe874f08f7ca5606f6a7a0769e92e9e7
dev-rhllor <dev-rhllor@protonmail.com>
2022-04-29 13:50:50 +0000

    fixing the files to match the same structure

--- PART4_DataFormats/data_import.py
+++ PART4_DataFormats/data_import.py
@@ -1,1 +1,37 @@
-# Here comes some code to import the different inventory files and manipulate them inside python.
+import json
+import yaml
+import xmltodict
+
+def json_importer():
+    with open('./PART4_DataFormats/inventory.json') as file:
+        inventory = json.load(file)
+        return inventory
+
+def yaml_importer():
+    with open('./PART4_DataFormats/inventory.yaml') as file:
+        inventory = yaml.load(file, Loader=yaml.FullLoader)
+        return inventory
+
+def xml_importer():
+    with open('./PART4_DataFormats/inventory.xml') as file:
+        xml_inventory = file.read()
+        inventory = xmltodict.parse(xml_inventory)
+        return inventory
+
+
+def main():
+    inventory_from_json = json_importer()
+    inventory_from_yaml = yaml_importer()
+    inventory_from_xml = xml_importer()
+    print(inventory_from_json['devices']['sandbox']['local'])
+    print(inventory_from_yaml['devices']['sandbox']['local'])
+    print(inventory_from_xml['devices']['sandbox']['local'])
+    print(inventory_from_xml['devices']['sandbox']['@id'])
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
