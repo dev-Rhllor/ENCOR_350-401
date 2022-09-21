e954795fc36c824787353eebdf3a717c1a1a0dbd
dev-rhllor <dev-rhllor@protonmail.com>
2022-09-21 06:53:39 +0000

    fixing naming of variable to match content

--- PART2_Paramiko_Netmiko_Napalm/netmiko_show.py
+++ PART2_Paramiko_Netmiko_Napalm/netmiko_show.py
@@ -8,11 +8,11 @@ def main():
     cisco_command = 'show interface'
     # junos_command = 'show interface'
     device_connection = ConnectHandler(**gns3_ios_router)
-    device_ip = device_connection.send_command(cisco_command)
-    print(device_ip)
+    device_interface = device_connection.send_command(cisco_command)
+    print(device_interface)
 
-    device_ip_parsed = device_connection.send_command(cisco_command, use_textfsm=True)
-    pprint(device_ip_parsed)
+    device_interface_parsed = device_connection.send_command(cisco_command, use_textfsm=True)
+    pprint(device_interface_parsed)
 
 
 if __name__ == "__main__":
