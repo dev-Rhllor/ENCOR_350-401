d4fa86f58e13ea6f7cc67ccf0961f7e425ee08fe
dev-rhllor <dev-rhllor@protonmail.com>
2022-10-05 08:57:05 +0000

    Changing example for consistency w Ansible Napalm

--- PART2_Paramiko_Netmiko_Napalm/netmiko_show.py
+++ PART2_Paramiko_Netmiko_Napalm/netmiko_show.py
@@ -1,18 +1,25 @@
+import json
 from pprint import pprint
 from netmiko import ConnectHandler
 from inventory import gns3_ios_router
-# from inventory import gns3_junos_vsrx
+from inventory import gns3_junos_vsrx
 
 
 def main():
-    cisco_command = 'show interface'
-    # junos_command = 'show interface'
+    ios_command = 'show ip interface brief'
     device_connection = ConnectHandler(**gns3_ios_router)
-    device_interface = device_connection.send_command(cisco_command)
-    print(device_interface)
+    interfaces = device_connection.send_command(ios_command)
+    print(interfaces)
 
-    device_interface_parsed = device_connection.send_command(cisco_command, use_textfsm=True)
-    pprint(device_interface_parsed)
+    interfaces_parsed = device_connection.send_command(ios_command, use_textfsm=True)
+    pprint(interfaces_parsed)
+
+
+    junos_command = 'show interface terse | display json'
+    device_connection = ConnectHandler(**gns3_junos_vsrx)
+    interfaces = device_connection.send_command(junos_command)
+    print(interfaces)
+    pprint(json.loads(interfaces))
 
 
 if __name__ == "__main__":
